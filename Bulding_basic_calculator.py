727e7f0835d864d28ac4d41c6d65ead92577c746
nahidrupok <nahidrupok581@gmail.com>
2026-06-24 19:01:16 +0600

    Calculator

---
+++ Bulding_basic_calculator.py
@@ -0,0 +1,4 @@
+num1=input("Enter a number:")
+num2=input("Enter another number:")
+result=float(num1)+float(num2) #we need to convert the input string to float before performing mathematical operations.
+print(result) 
