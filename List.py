5cdce05c9572369fc847e76fb142db78031027a1
nahidrupok <nahidrupok581@gmail.com>
2026-06-26 17:27:53 +0600

    Python List

---
+++ List.py
@@ -0,0 +1,9 @@
+friends=['Hasib', 'Rifat', 'Shuvo', 'Tuhin', 'Arif', 'Rony']
+#print(friends) 
+#print(friends[0])
+#print(friends[1:4])
+#print(friends[2:])
+#print(friends[-2])
+friends[2]="Rafi"
+print(friends[2])
+
