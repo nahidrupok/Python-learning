f0b23c350e9f7fffa96646b8ae25c542bd49f264
nahidrupok <nahidrupok581@gmail.com>
2026-06-26 17:52:15 +0600

    List functions

---
+++ List_function.py
@@ -0,0 +1,18 @@
+lucky_number=[5,7,9,3,2,0]
+#print(lucky_number)
+friends=["Hasib","Hasib", "Rifat", "Shuvo", "Tuhin", "Hasib","Arif", "Rony"]
+#friends.extend(lucky_number) #we can add two lists together using extend function
+#friends.append("Rafi") #adds an element to the end of the list
+#friends.insert(1,"Rajib") #adds an element to the list at a specific index
+#friends.remove("Rony") #removes an element from the list
+#friends.clear() #removes all elements from the list
+#friends.pop() #removes the last element from the list
+#print(friends.index("Shuvo"))  #to find the index of an element in the list
+#print(friends.count("Hasib")) #to count how many times an element appears in the list
+#friends.sort() #sorts the list in ascending order
+#print(friends)
+
+#lucky_number.sort() #sorts the list in ascending order
+#lucky_number.reverse() #reverses the order of the list
+lucky_number2=lucky_number.copy() #creates a copy of the list
+print(lucky_number2)
