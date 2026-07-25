afa0ed257b2d98fb5d099d5c1981fead957481dd
nahidrupok <nahidrupok581@gmail.com>
2026-07-25 15:53:15 +0600

    Return Statement

---
+++ Return_statement.py
@@ -0,0 +1,13 @@
+def cube(num):
+    return num*num*num
+
+result=cube(3)
+print(result)
+
+def get_name_and_age():
+    name="Hasib"
+    age="24"
+    return name,age
+person_name,person_age=get_name_and_age()
+print(person_name)
+print(person_age)
