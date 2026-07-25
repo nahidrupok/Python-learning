e6a096fe78de6f5dc21b3dda7cc326967bbed0ff
nahidrupok <nahidrupok581@gmail.com>
2026-07-25 07:49:30 +0600

    Tuple

---
+++ Tuples.py
@@ -0,0 +1,7 @@
+# Tuple is besicaly type of data structure.it store values.
+#Tuple is actually similar to list.
+#Tuple is immutable,it means tuple can be changed or modifird.
+
+coordinates=(4,5)
+
+print(coordinates[1])
