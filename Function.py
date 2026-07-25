cc4bb16ba7b84fa00bff44186966c83b1163fe4a
nahidrupok <nahidrupok581@gmail.com>
2026-07-25 11:20:24 +0600

    Function

---
+++ Function.py
@@ -0,0 +1,15 @@
+#function is besically collection of code which perform a specific task.
+#def say_hi():
+    #print("Hello Hasib")
+#print("Top")    
+#say_hi()
+#print("Bottom")
+
+
+#using parameter
+
+def say_hi(name,age):
+    print("Hello " + name+" you are "+ str(age))
+    
+say_hi("Hasib",35)
+say_hi("Saki",40)
